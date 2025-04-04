f988fa8f754e79da0ad2226e12f1da1445e1ab79
galilei <galilei.mail@gmail.com>
2025-04-03 19:52:42 -0700

    Add MONGO_URI to cloud run services

--- infrastructure/services/gcp_secrets.py
+++ infrastructure/services/gcp_secrets.py
@@ -22,3 +22,5 @@ def create_cloud_run_secret_env(secret_id):
 OPENAI_API_KEY = create_cloud_run_secret_env("OPENAI_API_KEY")
 ANTHROPIC_KEY = create_cloud_run_secret_env("ANTHROPIC_KEY")
 PERPLEXITY_KEY = create_cloud_run_secret_env("PERPLEXITY_KEY")
+AIRTABLE_API_KEY = create_cloud_run_secret_env("AIRTABLE_API_KEY")
+MONGODB_URI = create_cloud_run_secret_env("MONGODB_URI")
--- infrastructure/services/portfolio_service.py
+++ infrastructure/services/portfolio_service.py
@@ -3,7 +3,7 @@ import pulumi_gcp as gcp
 from .defaults import PROJECT_ID
 from .service_account import service_account
 from .repo import short_sha
-from .gcp_secrets import create_cloud_run_secret_env
+from .gcp_secrets import create_cloud_run_secret_env, AIRTABLE_API_KEY, MONGODB_URI
 
 
 portfolio = gcp.cloudrunv2.Service(
@@ -22,13 +22,14 @@ portfolio = gcp.cloudrunv2.Service(
                 image=f"us.gcr.io/{PROJECT_ID}/docker/synapse:{short_sha}",
                 args=['portfolio'],
                 envs=[
+                    AIRTABLE_API_KEY, MONGODB_URI,
                     gcp.cloudrunv2.ServiceTemplateContainerEnvArgs(
                         name="CLOUD",
                         value="1",
                     ),
                 ] + [
                     create_cloud_run_secret_env(secret_id = secret_id)
-                    for secret_id in ['COOKIE_SECRET', 'OAUTH_CLIENT_ID', 'OAUTH_SECRET', 'AIRTABLE_API_KEY']
+                    for secret_id in ['COOKIE_SECRET', 'OAUTH_CLIENT_ID', 'OAUTH_SECRET']
                 ],
                 resources=gcp.cloudrunv2.ServiceTemplateContainerResourcesArgs(
                     limits={
